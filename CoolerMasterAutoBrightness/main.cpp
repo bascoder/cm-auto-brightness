5e92f6a401e4fb4c461d8c0a1b42bd82731b2e11
Bas van Marwijk <basvmar@hotmail.com>
2020-10-31 12:26:10 +0100

    Set brightness based on current time

--- CoolerMasterAutoBrightness/main.cpp
+++ CoolerMasterAutoBrightness/main.cpp
@@ -1,25 +1,24 @@
 #include <iostream>
-#include <cstdio>
-#include <csignal>
+#include <ctime>
 #include "SDKDLL.h"
 #include "main.h"
 
 using namespace std;
 
-void ExitHandler() {
-	cout << "Handling exit" << endl;
-
-	Cleanup();
-}
+const int DAWN = 8;
+const int DUSK = 17;
+const int BRIGHTNESS_DARK = 50;
+const int BRIGHTNESS_BRIGHT = 200;
 
 int main() {
 	cout << "Cooler Master controller" << endl << endl;
 
-
 	try
 	{
 		Init();
-		SetBrightness(5);
+
+		auto brightness = GetPreferedBrightness();
+		SetBrightness(brightness);
 
 		AwaitExit();
 	}
@@ -44,6 +43,32 @@ void Init()
 	}
 }
 
+BYTE GetPreferedBrightness()
+{
+	int hour = GetCurrentHour();
+
+	if (hour > DUSK && hour < DAWN) {
+		return BRIGHTNESS_DARK;
+	}
+	else {
+		return BRIGHTNESS_BRIGHT;
+	}
+}
+
+int GetCurrentHour()
+{
+	time_t ttime = time(0);
+	struct tm localTime;
+	errno_t err = localtime_s(&localTime, &ttime);
+
+	if (err) {
+		cerr << "Could not obtain local time" << endl;
+		throw new exception("Failed to obtain localTime", err);
+	}
+
+	return localTime.tm_hour;
+}
+
 void SetBrightness(BYTE brightness)
 {
 	if (!SetFullLedColor(brightness, brightness, brightness)) {
@@ -54,7 +79,7 @@ void SetBrightness(BYTE brightness)
 void AwaitExit()
 {
 	cout << "Press Any Key To Exit..." << endl;
-	getchar();
+	cin.get();
 }
 
 void Cleanup()
@@ -62,4 +87,10 @@ void Cleanup()
 	if (!EnableLedControl(false)) {
 		cerr << "Failed to give back LED control!";
 	}
+}
+
+void ExitHandler() {
+	cout << "Handling exit" << endl;
+
+	Cleanup();
 }
--- CoolerMasterAutoBrightness/main.h
+++ CoolerMasterAutoBrightness/main.h
@@ -4,6 +4,10 @@ void Cleanup();
 
 void AwaitExit();
 
+BYTE GetPreferedBrightness();
+
+int GetCurrentHour();
+
 void SetBrightness(BYTE brightness);
 
 void Init();
